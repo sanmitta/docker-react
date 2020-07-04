1a3d0305cadae938066524c4dc1fcaa8843cfa92
sanmitta <sanmitta@gmail.com>
2020-07-03 23:09:09 -0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SMX kjhikhkDivya!: Edit <code>src/App.js</code> and save to reload.
+          SMX I moved to feature branch: Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
